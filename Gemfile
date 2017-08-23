23fb5a44144ded7d8461b4ef9f4e7066d58f3896
Elizabeth Chan <elizabeth.schan@outlook.com>
2017-08-23 13:07:52 +0100

    new database hobby-dev

--- Gemfile
+++ Gemfile
@@ -17,4 +17,5 @@ group :test do
   gem 'dm-postgres-adapter'
   gem 'database_cleaner'
   gem 'rerun'
+  gem 'pg'
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -12,6 +12,7 @@ end
 
 # A Postgres connection:
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+# DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 
 DataMapper.finalize
 DataMapper.auto_upgrade!
